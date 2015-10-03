9c80fa6e5801f1313e3006b8e075d86ed33c6522
ginvaell <ginvaell@gmail.com>
2015-10-04 01:12:38 +0300

    task1_4

---
+++ src/com/ginvaell/module1/task4/Main.java
@@ -0,0 +1,18 @@
+package com.ginvaell.module1.task4;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    //lab 1-4
+        double[] a = {1, 2, 20, 0, 5, 3};
+        double max = Double.MIN_VALUE;
+        int n = a.length;
+        for (int i = 0; i < n/2; i++) {
+            if(a[i]+a[n-1-i] > max) {
+                max = a[i]+a[n-1-i];
+            }
+        }
+        System.out.print(max);
+
+    }
+}
