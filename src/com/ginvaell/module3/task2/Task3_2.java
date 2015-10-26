11155b6ff42e87b04722d37c10f6a0c392d9827a
ginvaell <ginvaell@gmail.com>
2015-10-26 18:56:51 +0300

    task3-2 encoding

--- src/com/ginvaell/module3/task2/Task3_2.java
+++ src/com/ginvaell/module3/task2/Task3_2.java
@@ -8,7 +8,7 @@ public class Task3_2 {
     public static void main(String[] args) {
         try (Scanner in = new Scanner(System.in)) {
             System.out.println("1. English");
-            System.out.println("2. Р СѓСЃСЃРєРёР№");
+            System.out.println("2. Русский");
 
             if (in.hasNextInt()) {
                 int l = in.nextInt();
